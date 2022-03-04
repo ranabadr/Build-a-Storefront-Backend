9e67921b1675d29e74c20db3dab62a4a40451efd
Rana Badr <rana_badr30@yahoo.com>
2022-03-04 15:27:25 +0200

    Add the method to attach a product to an order

--- src/models/order.ts
+++ src/models/order.ts
@@ -74,4 +74,22 @@ export class OrderStore {
           throw new Error(`Could not delete order ${id}. Error: ${err}`)
       }
   }
+
+  async addProduct(quantity: number, orderId: string, productId: string): Promise<Order> {
+    try {
+      const sql = 'INSERT INTO order_products (quantity, order_id, product_id) VALUES($1, $2, $3) RETURNING *'
+      
+      const conn = await client.connect()
+
+      const result = await conn.query(sql, [quantity, orderId, productId])
+
+      const order = result.rows[0]
+
+      conn.release()
+
+      return order
+    } catch (err) {
+      throw new Error(`Could not add product ${productId} to order ${orderId}: ${err}`)
+    }
+  }
 }
