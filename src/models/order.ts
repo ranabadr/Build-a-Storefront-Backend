c4e57b755ca9f658f6a5cb594352eec77d34d652
Rana Badr <rana_badr30@yahoo.com>
2022-03-04 15:22:23 +0200

    Create a model for orders table

---
+++ src/models/order.ts
@@ -0,0 +1,77 @@
+import client from '../database';
+
+export type Order = {
+     id: number;
+     status: string;
+     user_id: number;
+}
+
+export class OrderStore {
+  async index(): Promise<Order[]> {
+    try {
+      const conn = await client.connect()
+      const sql = 'SELECT * FROM orders'
+  
+      const result = await conn.query(sql)
+  
+      conn.release()
+  
+      return result.rows 
+    } catch (err) {
+      throw new Error(`Could not get orders. Error: ${err}`)
+    }
+  }
+
+  async show(id: string): Promise<Order> {
+    try {
+    const sql = 'SELECT * FROM orders WHERE id=($1)'
+   
+    const conn = await client.connect()
+
+    const result = await conn.query(sql, [id])
+
+    conn.release()
+
+    return result.rows[0]
+    } catch (err) {
+        throw new Error(`Could not find ordes ${id}. Error: ${err}`)
+    }
+  }
+
+  async create(o: Order): Promise<Order> {
+      try {
+    const sql = 'INSERT INTO orders (status, user_id) VALUES($1, $2) RETURNING *'
+    
+    const conn = await client.connect()
+
+    const result = await conn
+        .query(sql, [o.status, o.user_id])
+
+    const order = result.rows[0]
+
+    conn.release()
+
+    return order
+      } catch (err) {
+          throw new Error(`Could not add new order ${o.status}. Error: ${err}`)
+      }
+  }
+
+  async delete(id: string): Promise<Order> {
+      try {
+    const sql = 'DELETE FROM orders WHERE id=($1)'
+   
+    const conn = await client.connect()
+
+    const result = await conn.query(sql, [id])
+
+    const order = result.rows[0]
+
+    conn.release()
+
+    return order
+      } catch (err) {
+          throw new Error(`Could not delete order ${id}. Error: ${err}`)
+      }
+  }
+}
