2d4c7e036a40fe73ac08ae90b394b8abbeb3801e
Rana Badr <rana_badr30@yahoo.com>
2022-03-04 15:36:05 +0200

    Create CORS configration options

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: express.Application = express();
 const address: string = '0.0.0.0:3000';
 
+const corsOptions = {
+    origin: 'http://someotherdomain.com',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.get('/', function (req: Request, res: Response) {
