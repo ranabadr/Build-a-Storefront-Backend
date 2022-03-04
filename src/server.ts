d50b7cf077204bfaccf15133a38d4bd3c265f1e5
Rana Badr <rana_badr30@yahoo.com>
2022-03-04 15:41:21 +0200

    Create a route to products model

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import productRoutes from './handlers/products'
 
 const app: express.Application = express();
 const address: string = '0.0.0.0:3000';
@@ -16,6 +17,8 @@ app.use(bodyParser.json());
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!');
 });
+
+productRoutes(app);
 
 app.listen(3000, function () {
     console.log(`starting app on: ${address}`);
