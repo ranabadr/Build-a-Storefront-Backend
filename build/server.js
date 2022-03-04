2173a80b83ebd184fff5899fae6a7804d501e843
Rana Badr <rana_badr30@yahoo.com>
2022-03-04 16:18:19 +0200

    Run build

--- build/server.js
+++ build/server.js
@@ -5,12 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
+var products_1 = __importDefault(require("./handlers/products"));
+var users_1 = __importDefault(require("./handlers/users"));
+var orders_1 = __importDefault(require("./handlers/orders"));
 var app = (0, express_1.default)();
 var address = '0.0.0.0:3000';
+var corsOptions = {
+    origin: 'http://someotherdomain.com',
+    optionsSuccessStatus: 200
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use(body_parser_1.default.json());
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+(0, products_1.default)(app);
+(0, users_1.default)(app);
+(0, orders_1.default)(app);
 app.listen(3000, function () {
     console.log("starting app on: ".concat(address));
 });
