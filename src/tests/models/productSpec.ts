360eed26d97af3822436d96bb66d5e8e7529812d
Rana Badr <rana_badr30@yahoo.com>
2022-03-06 14:05:47 +0200

    Create a sepc for product method

---
+++ src/tests/models/productSpec.ts
@@ -0,0 +1,66 @@
+import { Product, ProductStore } from '../../models/product';
+
+const store = new ProductStore()
+
+describe("Product Model", () => {
+  it('should have an index method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have a show method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have a create method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have a update method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have a delete method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('create method should add a product', async () => {
+    const result = await store.create({
+        id: 1,
+        name: 'Adidas',
+        price: 250,
+        category: 'Sporty shoes'
+    });
+    expect(result).toEqual({
+      id: 1,
+      name: 'Adidas',
+      price: 250,
+      category: 'Sporty shoes'
+    });
+  });
+
+  it('index method should return a list of products', async () => {
+    const result = await store.index();
+    expect(result).toEqual([{
+      id: 1,
+      name: 'Adidas',
+      price: 250,
+      category: 'Sporty shoes'
+    }]);
+  });
+
+  it('show method should return the correct product', async () => {
+    const result = await store.show("1");
+    expect(result).toEqual({
+        id: 1,
+        name: 'Adidas',
+        price: 250,
+        category: 'Sporty shoes'
+  });
+
+  it('delete method should remove the product', async () => {
+    store.delete("1");
+    const result = await store.index()
+
+    expect(result).toEqual([]);
+  });
+});
