410ae27d3a44fd3044b36f6a8067dbf53669c4d0
Rana Badr <rana_badr30@yahoo.com>
2022-03-06 15:11:59 +0200

    Update the userSpec.ts file

--- src/tests/models/userSpec.ts
+++ src/tests/models/userSpec.ts
@@ -8,19 +8,15 @@ describe("User Model", () => {
   });
 
   it('should have a show method', () => {
-    expect(store.index).toBeDefined();
+    expect(store.show).toBeDefined();
   });
 
   it('should have a create method', () => {
-    expect(store.index).toBeDefined();
-  });
-
-  it('should have a update method', () => {
-    expect(store.index).toBeDefined();
+    expect(store.create).toBeDefined();
   });
 
   it('should have a delete method', () => {
-    expect(store.index).toBeDefined();
+    expect(store.delete).toBeDefined();
   });
 
   it('create method should add a user', async () => {
