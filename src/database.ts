66af42e0e1e70c066436117cc667b9016993fb6d
Rana Badr <rana_badr30@yahoo.com>
2022-03-06 13:23:29 +0200

    Allow connecting to the test or dev database

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,36 @@
-import dotenv from 'dotenv';
-import {Pool} from 'pg';
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
 
-dotenv.config();
+dotenv.config()
 
 const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-}= process.env;
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env
 
-const client = new Pool({
+let client
+console.log(ENV)
+
+if(ENV === 'test') {
+  client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DB,
     user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD
-})
+    password: POSTGRES_PASSWORD,
+  })
+}
 
-export default client;
+if(ENV === 'dev') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  })
+}
+
+export default client
