f33a69c1d54ad4fd171238798040328db5c32f48
Rana Badr <rana_badr30@yahoo.com>
2022-03-06 21:01:55 +0200

    Add verifyAuthToken for all order routes

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -7,6 +7,18 @@ dotenv.config();
 
 const store = new UserStore();
 
+const verifyAuthToken = (req: Request, res: Response, next: () => void) => {
+    try {
+      const authorizationHeader = req.headers.authorization as string;
+      const token = authorizationHeader;
+      jwt.verify(token, process.env.TOKEN_SECRET as string);
+      next();
+    } catch (error) {
+      res.status(401).json('Access denied, invalid token');
+    }
+  };
+  
+
 const index = async (_req: Request, res: Response) => {
   const users = await store.index()
   res.json(users)
@@ -27,8 +39,7 @@ const create = async (req: Request, res: Response) => {
         }
 
         const newUser = await store.create(user)
-        const token: string = jwt.sign({user: newUser}, process.env.TOKEN_SECRET as string)
-        res.json(token)
+        res.json(newUser)
     } catch(err) {
         res.status(400)
         res.json(err)
@@ -41,10 +52,10 @@ const destroy = async (req: Request, res: Response) => {
 }
 
 const userRoutes = (app: express.Application) => {
-  app.get('/users', index)
-  app.get('/users/:id', show)
-  app.post('/users', create)
-  app.delete('/users', destroy)
+  app.get('/users', verifyAuthToken, index)
+  app.get('/users/:id', verifyAuthToken, show)
+  app.post('/users', verifyAuthToken, create)
+  app.delete('/users', verifyAuthToken, destroy)
 }
 
 export default userRoutes;
