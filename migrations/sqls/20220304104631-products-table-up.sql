8ce8d85e321db26dc351bcd8771f3a10669b2ae5
Rana Badr <rana_badr30@yahoo.com>
2022-03-11 00:42:22 +0200

    Delete migrations

--- migrations/sqls/20220304104631-products-table-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR,
-    price integer,
-    category VARCHAR
-);
--- migrations/sqls/20220304105141-users-table-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    firstName VARCHAR,
-    lastName VARCHAR,
-    password VARCHAR
-);
